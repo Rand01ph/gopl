4ee3c7080a4f8f175f970e09f19259b5331f2912
Rand01ph <tanyawei1991@gmail.com>
2018-08-29 18:08:33 +0800

    完成第一章习题

---
+++ ch1/ex1.12.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	u, err := url.Parse(r.URL.RawPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	q := u.Query()
+	cycles := 5
+	if q.Get("cycles") != "" {
+		cycles, err = strconv.Atoi(q.Get("cycles"))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	lissajous(w, cycles)
+}
+
+var palette = []color.Color{color.White, color.Black, color.RGBA{0, 128, 0, 1}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+	greenIndex = 2
+)
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
